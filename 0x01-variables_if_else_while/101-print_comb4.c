95aefd3b3c8929dbd37c4889ba06a2192aada8ee
julius Kaboyo <juliolandsat@gmail.com>
2023-03-20 17:29:04 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,11 +13,17 @@ for (m = 49; m < 58; m++)
 for (l = 50; l < 58; l++)
 {
 if (l > m && m > n)
-{                                    putchar(n);
+{                     	
+putchar(n);
 putchar(m);
-putchar(l);                          if (n != 55 || m != 56)
-{                                    putchar(',');
-putchar(' ');                        }
+putchar(l);
+if (n != 55 || m != 56)
+{                       
+
+putchar(',');
+putchar(' ');
+
+}
 }
 }
 }
